f9eb4722864d9cd0b70eea113c0c1171cff26218
Ashhad <mianashhad7@gmail.com>
2023-12-14 00:53:23 +0500

    Created classes and a couple of functions for the shell commands

--- DOS_Shell/DOS_Shell/Shell.h
+++ DOS_Shell/DOS_Shell/Shell.h
@@ -1,30 +1,99 @@
 #pragma once
 
-template <typename T>
+#include <list>
+
+using namespace std;
 
 class Shell
 {
-	struct File
-	{
-
-	};
-	struct Directory
-	{
-		vector<Directory> dirs;
-		vector<File> files;
-	};
+public:
 	struct FileStructure
 	{
-		struct Node
+		struct File
 		{
-			T data;
-			vector<Node*> 
+			string name;
+			string data;
+			string extension;
+			int size;
 		};
+
+		struct Directory
+		{
+			string name;
+			string path;
+
+			list<Directory> directories;
+			list<File> files;
+			Directory* parent;
+
+			Directory()
+			{
+				name = "";
+				path = "V:\\>";
+				parent = nullptr;
+			}
+
+			Directory(string n, string p, Directory* par)
+			{
+				name = n;
+				path = p;
+				parent = par;
+			}
+		};
+		Directory* rootDirectory;
+		Directory* currentDirectory;
+
+		FileStructure()
+		{
+			rootDirectory = new Directory();
+			currentDirectory = rootDirectory;
+		}
 	};
-private:
-	Node* root;
+
+	FileStructure virtualFileStructure;
+
 	Shell()
 	{
+		virtualFileStructure = FileStructure();
+	}
 
+	void PrintAuthorInfo()
+	{
+		cout << "Authors: Mian Muhammad Ashhad, Muhammad Saad Akmal" << endl;
+		cout << "Registration Numbers: 2022-CS-114, 2022-CS-148" << endl;
+		cout << "Course: Data Structures and Algorithms" << endl;
+		cout << endl << endl;
+	}
+
+	void DisplayCurrentPath()
+	{
+		cout << virtualFileStructure.currentDirectory->path;
+	}
+
+	void Help()
+	{
+		cout << "cd. : Displays working directory";
+		cout << "pwd : Displays working directory";
+	}
+
+	void PWD()
+	{
+		cout << "Current Directory: \"";
+		cout << virtualFileStructure.currentDirectory->path << "\"";
+		cout << endl;
+	}
+
+	void ExecuteCommand(string command)
+	{
+		if (command == "pwd" || command == "cd.")
+		{
+			PWD();
+		}
+
+		else
+		{
+			cout << "No such command found: \"" << command << "\"";
+			cout << endl;
+		}
 	}
 };
